b9f407ba8c5a801817737b55942285e552d72095
AmiT <43564590+amitvadhel@users.noreply.github.com>
2020-10-27 13:16:25 +0300

    Return -1 instead of False if NID is not valid

--- validateSAID.py
+++ validateSAID.py
@@ -1,12 +1,13 @@
 def check(id):
+    not_valid = -1
     id = id.strip()
     if (id.isdigit() is False):
-        return False
+        return not_valid
     if (len(id) != 10):
-        return False
+        return not_valid
     type = id[0:1]
     if (type != '2' and type != '1'):
-        return False
+        return not_valid
     sum = 0
     for i in range(0, 10):
         if (i % 2 == 0):
@@ -14,5 +15,5 @@ def check(id):
             sum += int(ZFOdd[0]) + int(ZFOdd[1])
         else:
             sum += int(id[i])
-    isValid = -1 if sum % 10 != 0 else  type
+    isValid = not_valid if sum % 10 != 0 else  type
     return isValid
