81b0dad0e6467dbc7c84d00132363285f94617f4
Siddharth Jain <jainnsidhearth77@gmail.com>
2023-06-17 00:02:06 +0530

    Refactor code for JS
    - Readability
    - Latest ES standards supported
    - Remove var
    - better naming

--- validateSAID.js
+++ validateSAID.js
@@ -3,25 +3,23 @@
  * @return {number}
  */
 function validateSAID(id) {
-  
+  let type = id.substr(0, 1);
+  let sum = 0;
+  const _idLength = 10;
+  const _type1 = '1';
+  const _type2 = '2';
   id = id.trim();
-  if (isNaN(parseInt(id))) {
+  if (isNaN(parseInt(id)) || (id.length !== _idLength) || (type !== _type2 && type !== _type1)) {
     return -1;
   }
-  if (id.length !== 10) {
-    return -1;
-  }
-  var type = id.substr(0, 1);
-  if (type !== '2' && type !== '1') {
-    return -1;
-  }
-  var sum = 0;
-  for (var i = 0; i < 10; i++) {
-    if (i % 2 === 0) {
-      var ZFOdd = String('00' + String(Number(id.substr(i, 1)) * 2)).slice(-2);
-      sum += Number(ZFOdd.substr(0, 1)) + Number(ZFOdd.substr(1, 1));
+  for (let num = 0; num < 10; num++) {
+    const digit = Number(id[num]);
+    if (num % 2 === 0) {
+      const doubled = digit * 2;
+      const ZFOdd = `00${doubled}`.slice(-2);
+      sum += Number(ZFOdd[0]) + Number(ZFOdd[1]);
     } else {
-      sum += Number(id.substr(i, 1));
+      sum += digit;
     }
 
   }
